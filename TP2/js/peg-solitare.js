9231584fd4eccb259f8e06200a72fbb4f06b31a2
abril troncoso <abrilpotitroncoso@gmail.com>
2025-10-07 23:25:27 -0300

    comandos

--- TP2/js/peg-solitare.js
+++ TP2/js/peg-solitare.js
@@ -1,7 +1,7 @@
-const form = document.querySelector('#comments form');
+const form = document.querySelector("#comments form");
 
-const comment=form.querySelector("input[name='comentario']");
-const commentList = document.getElementById('comment-list'); 
+const comment = form.querySelector("input[name='comentario']");
+const commentList = document.getElementById("comment-list");
 const botonEnviar = document.querySelector("#comments button");
 
 comment.addEventListener("focus", () => {
@@ -12,15 +12,15 @@ comment.addEventListener("blur", () => {
     botonEnviar.style.display = "none";
   }
 });
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const comment = form.elements['comentario'];
-    const commentText = comment.value.trim();  
-    if (commentText !== '') {
-        const newcomment = document.createElement('div');
-        newcomment.className = 'comment';
-        newcomment.innerHTML = `
+  const comment = form.elements["comentario"];
+  const commentText = comment.value.trim();
+  if (commentText !== "") {
+    const newcomment = document.createElement("div");
+    newcomment.className = "comment";
+    newcomment.innerHTML = `
        <div class="comment-new-header">
                <i class="fa-solid fa-user"></i>
                <span>Usuario Anónimo</span>
@@ -35,9 +35,31 @@ form.addEventListener('submit', function(event) {
                  <a href="#">Responder</a>
         </div>
                 `;
-        commentList.appendChild(newcomment);
-        comment.value = ''; 
-        comment.focus();
-         botonEnviar.style.display = "none";
-    }});
+    commentList.appendChild(newcomment);
+    comment.value = "";
+    comment.focus();
+    botonEnviar.style.display = "none";
+  }
+});
 
+// Obtener elementos
+const jostick = document.getElementById("jostick");
+const modal = document.getElementById("modalControles");
+const cerrar = document.querySelector(".cerrar-modal");
+
+// Abrir modal al hacer clic en el joystick
+jostick.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+// Cerrar modal al hacer clic en la X
+cerrar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Cerrar modal al hacer clic fuera del contenido
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
